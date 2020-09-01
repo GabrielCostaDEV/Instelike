72a3d4243236aa9e786e3168fd87dbbafce8ce58
GabrielCostaDEV <gabriel.2019012538@aluno.iffar.edu.br>
2020-09-01 10:59:44 -0300

    Classes

---
+++ InstagramBot.py
@@ -0,0 +1,332 @@
+import time
+from random import randint
+
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+class InstagramBot:
+    def __init__(self, listaPerfis, caminho):
+        self.listaPerfis = listaPerfis
+        self.caminho = caminho
+        self.driver = webdriver.Firefox(executable_path = self.caminho)
+        self.logado = False
+        self.trocar = False
+        self.perfil = 0
+        self.contador = 0
+        self.abas = list()
+        self.perfis = list()
+
+    def pegarUrl(self): #segunda aba (pegar url)
+        driver = self.driver
+        time.sleep(randint(2, 5))
+        #Se for uma publicação:
+        if '/p' in driver.current_url:
+            time.sleep(randint(2, 5))
+            tag = driver.find_elements_by_tag_name('a')
+            hrefs = [elem.get_attribute('href') for elem in tag]
+
+            url = hrefs[0]
+        else:    
+            time.sleep(randint(2, 5))
+            #Pegando a url do instagram:
+            url = driver.current_url
+        nova = url[26:(len(url)-1)]
+        return nova
+
+    def buscar_xpath(self, xpath):
+        self.xpath = xpath
+        driver = self.driver
+        while True:
+            try:
+                driver.find_element_by_xpath(self.xpath).click()
+                break
+            except:
+                driver.refresh()
+                time.sleep(5)
+            return True  
+
+    def buscar_classe(self, classe):
+        self.classe = classe
+        driver = self.driver
+        while True:
+            try:
+                driver.find_element_by_class_name(self.classe).click()
+                break
+            except:
+                driver.refresh()
+                time.sleep(5)
+            return True
+
+    def logarInsta(self):
+        driver = self.driver
+        url = self.pegarUrl() #pegar url
+        driver.get("https://www.instagram.com/") #terceira aba (login no instagram)
+        time.sleep(randint(2, 5))
+
+        #trocar perfil
+        if self.trocar:
+            self.buscar_classe('HoLwm')
+            time.sleep(randint(2, 5))
+            driver.find_element_by_class_name('gmFkV').click()
+            time.sleep(randint(2, 5))
+            sair = self.buscar_classe('wpO6b')
+            time.sleep(randint(2, 5))
+            self.buscar_xpath('//button[text()="Sair"]')
+
+            self.logado = False
+            self.trocar = False
+
+        time.sleep(randint(2, 5))
+
+        if self.logado == False:
+            #Input de dados
+            while True:
+                try:
+                    campo_username = driver.find_element_by_xpath("//input[@name='username']")
+                    campo_username.click()
+                    time.sleep(randint(2, 5))
+                    campo_username.send_keys(self.listaPerfis[self.contador])
+
+                    campo_password = driver.find_element_by_xpath("//input[@name='password']")
+                    campo_password.click()
+                    time.sleep(randint(2, 5))
+                    campo_password.send_keys(self.listaPerfis[self.contador+1])
+                    
+                    time.sleep(randint(2, 5))
+                    campo_password.send_keys(Keys.RETURN)
+                    break
+                except:
+                    self.logarInsta()
+            
+            #Não salvar:
+            time.sleep(randint(2, 5))  
+            self.buscar_xpath("/html/body/div[1]/section/main/div/div/div/div/button")
+            
+            #Ativar notificações:
+            time.sleep(randint(2, 5))
+            self.buscar_classe('HoLwm')   
+            self.logado = True  
+            self.contador += 2
+
+        #Pesquisar
+        while True:
+            try:
+                time.sleep(randint(2, 5))
+                buscar = driver.find_element_by_class_name('TqC_a')
+                buscar.click()
+                time.sleep(randint(2, 5))
+                buscar = driver.find_element_by_xpath('//input[@data-focus-visible-added=""]')
+                buscar.click()
+                time.sleep(randint(2, 5))
+                buscar.clear()
+                time.sleep(randint(2, 5))
+                buscar.send_keys(url)
+                time.sleep(randint(1, 5))
+                buscar = driver.find_element_by_xpath('//a[@href="/' + url + '/"]').click()
+                time.sleep(randint(1, 5))
+                break
+            except:
+                self.logarInsta()
+                time.sleep(5)
+
+        if driver.current_url != ('https://www.instagram.com/' + url + '/'): #se a url de pesquisa for igual a url referência:
+            driver.get('https://www.instagram.com/' + url + '/')
+        #Clicando em seguir:
+        try:
+            seguir = driver.find_element_by_class_name('_6VtSN').click()
+            time.sleep(randint(1, 5))
+        except:
+            return False
+
+        try:
+            driver.find_element_by_class_name('HoLwm').click()
+            time.sleep(randint(1, 5))
+            driver.refresh()
+        except:
+            return False
+
+        try:
+            driver.find_element_by_xpath('//button[text()="OK"]').click()
+            time.sleep(5)
+            driver.refresh()
+        except:
+            return False   
+
+    def loginInstelike(self):
+        driver = self.driver
+        #Primeira aba (login no instelikes)
+        driver.get("https://instelikes.com.br/app?share=REF-hukeAAAAAAA#login")
+        self.abas.append(driver.window_handles[0])
+        driver.switch_to_window(self.abas[0])
+
+        #Input de dados:
+        try:
+            time.sleep(3)
+            campo_email = driver.find_element_by_xpath('//input[@name="email"]')
+            campo_email.click()
+            campo_email.send_keys(self.listaPerfis[self.contador])
+            campo_email.send_keys(Keys.RETURN)
+            time.sleep(3)
+            campo_senha = driver.find_element_by_xpath('//input[@name="password"]').click()
+            campo_senha.send_keys(self.listaPerfis[self.contador+1])
+            campo_senha.send_keys(Keys.RETURN)
+        except:
+            driver.refresh()
+            time.sleep(3)
+            self.abas.clear()
+            self.contador=0
+            self.loginInstelike()
+
+        #Selecionar perfil:
+        time.sleep(3)
+        self.buscar_xpath("/html/body/main/x-active-template/div/div/div/div/div/div/form/x-active-template/div[1]/div[1]/div")
+
+        #Verificar o número de perfis:
+        time.sleep(3)
+        cont = 1
+        path = self.checarBotao('/html/body/aside[1]/div[2]/ul[2]/li[4]/div/div/div/div[2]/a['+str(cont)+']/div')
+        while path:
+            self.perfis.append('/html/body/aside[1]/div[2]/ul[2]/li[4]/div/div/div/div[2]/a['+str(cont)+']/div')     
+            cont += 1 
+            path = self.checarBotao('/html/body/aside[1]/div[2]/ul[2]/li[4]/div/div/div/div[2]/a['+str(cont)+']/div')     
+        
+        print(f'Número de perfis: {len(self.perfis)}')
+        
+        #Botão ganhe moedas: 
+        self.buscar_xpath('//a[@data-text="earn-coins"]')
+
+    def entrarDireto(self):
+        driver = self.driver
+        try:
+            driver.find_element_by_class_name('_6VtSN').click()
+            time.sleep(randint(1, 5))
+        except:
+            return False
+        return True
+
+        try:
+            driver.find_element_by_class_name('HoLwm').click()
+            time.sleep(randint(1, 5))
+        except:
+            return False
+        return True    
+
+        try:
+            driver.find_element_by_xpath('//button[text()="OK"]').click()
+            time.sleep(randint(1, 5))
+            driver.refresh()
+        except:
+            return False        
+        return True    
+
+    def trocarPerfil(self):
+        driver = self.driver
+        if len(self.abas) >= 1:    
+            driver.switch_to_window(self.abas[0])
+        print('abas: ',self.abas)
+        time.sleep(1)
+        try:
+            driver.find_element_by_xpath('//button[@data-text="confirm"]').click()    
+            time.sleep(1)
+        except:
+            time.sleep(1)
+
+        if len(self.abas) >= 1:    
+            self.abas.pop()                
+        time.sleep(1)
+        driver.refresh()
+        time.sleep(2)    
+        print('abas: ',self.abas)  
+
+        try:       
+            time.sleep(1)
+            self.buscar_xpath('/html/body/aside[1]/div[2]/ul[2]/li[4]/div/img')
+            time.sleep(1)
+            if self.perfil >= (len(self.perfis)-1):
+                self.perfil = 0
+            else:
+                self.perfil += 1
+            self.buscar_xpath(self.perfis[self.perfil])
+            time.sleep(1)
+            self.trocar = True
+        except:
+            self.trocarPerfil()
+            time.sleep(5)            
+
+    def selecionarPerfil(self):
+        driver = self.driver
+        if self.logado:
+            #Voltar a página anterior:
+            if len(self.abas) > 1:    
+                driver.switch_to_window(self.abas[0])                
+            try:
+                driver.find_element_by_xpath('//button[@data-text="confirm"]').click()
+                time.sleep(2)
+            except:
+                time.sleep(2)
+
+            if len(self.abas) > 1:    
+                self.abas.pop()                
+            time.sleep(5) 
+
+            if (self.checarBotao('/html/body/main/x-active-template/div/div/div[2]/div/div/div/div[3]/div/div[2]/div[2]/form') == False) or (self.trocar == True):
+                try:
+                    time.sleep(1)
+                    self.buscar_xpath('/html/body/main/x-active-template/div/div/div[2]/form/div/div/div[1]')
+                    time.sleep(1)
+                    self.buscar_xpath('/html/body/main/x-active-template/div/div/div[2]/form/div/div/div[3]/div/label[2]/input')
+                    time.sleep(1)
+                    self.buscar_xpath('/html/body/main/x-active-template/div/div/div[2]/form/button')
+                    time.sleep(1)
+                    #Botão Ganhe moedas
+                    self.buscar_xpath('/html/body/main/x-active-template/div/div/div[2]/div/div/div/div[3]/div/div[2]/div[2]/form')
+                except:
+                    self.selecionarPerfil()
+                    time.sleep(5)
+            else:
+                self.buscar_xpath('/html/body/main/x-active-template/div/div/div[2]/div/div/div/div[3]/div/div[2]/div[2]/form')
+            time.sleep(3)
+        
+        else:
+            try:
+                time.sleep(1)
+                self.buscar_xpath('/html/body/main/x-active-template/div/div/div[2]/form/div/div/div[1]')
+                time.sleep(1)
+                self.buscar_xpath('/html/body/main/x-active-template/div/div/div[2]/form/div/div/div[3]/div/label[2]/input')
+                time.sleep(1)
+                self.buscar_xpath('/html/body/main/x-active-template/div/div/div[2]/form/button')
+                time.sleep(1)
+                #Botão Ganhe moedas
+                self.buscar_xpath('/html/body/main/x-active-template/div/div/div[2]/div/div/div/div[3]/div/div[2]/div[2]/form')
+            except:
+                self.selecionarPerfil()
+                time.sleep(5)
+
+        while True:
+            try:
+                self.abas.append(driver.window_handles[1])
+                driver.switch_to_window(self.abas[1])
+                break
+            except:
+                self.selecionarPerfil()
+                time.sleep(5)       
+
+    def checarBotao(self, xpath):
+        self.xpath = xpath
+        driver = self.driver
+        try:
+            driver.find_element_by_xpath(self.xpath)
+        except:
+            return False    
+        return True    
+
+    def getSessao(self):
+        if self.logado:
+            return True
+        else:
+            return False     
